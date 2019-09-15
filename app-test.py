5bf6e521e924ea1512fbb64f0b951de41c97921d
Andrew <andrew.j.price@outlook.com>
2019-09-15 15:44:37 -0700

    add login/logout routes

--- app-test.py
+++ app-test.py
@@ -1,16 +1,66 @@
 import unittest
 import os
-from app import app
+import tempfile
+import app
 
 class BasicTestCase(unittest.TestCase):
     def test_index(self):
-        tester = app.test_client(self)
+        tester = app.app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
     
     def test_database(self):
         tester = os.path.exists('blog.db')
         self.assertTrue(tester)
+
+class FlaskrTestCase(unittest.TestCase):
+    def setUp(self):
+        """Set up a blank temp database before each test."""
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        app.init_db()
+
+    def tearDown(self):
+        """Destroy blank temp database after each test."""
+        os.close(self.db_fd)
+        os.unlink(app.app.config['DATABASE'])
+
+    def login(self, username, password):
+        """Login helper function."""
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        """Logout helper function."""
+        return self.app.get('/logout', follow_redirects=True)
+
+    # assert functions
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'No entries' in rv.data
+
+    def test_login_logout(self):
+        rv = self.login(
+            app.app.config['USERNAME'],
+            app.app.config['PASSWORD']
+        )
+        assert b'Successfully logged in' in rv.data
+        rv = self.logout()
+        assert b'Successfully logged out' in rv.data
+        rv = self.login(
+            app.app.config['USERNAME'] + 'x',
+            app.app.config['PASSWORD']
+        )
+        assert b'Invalid username' in rv.data
+        rv = self.login(
+            app.app.config['USERNAME'],
+            app.app.config['PASSWORD'] + 'x'
+        )
+        assert b'Invalid password' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template, session, redirect, url_for, request, flash, abort
 
 # configs
 DATABASE = 'blog.db'
@@ -37,6 +37,35 @@ def get_db():
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
+@app.route('/')
+def index():
+    db = get_db()
+    query = db.execute('select * from posts order by id desc')
+    posts = query.fetchall()
+    return render_template('index.html', posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('Successfully logged in')
+            return redirect(url_for('index'))
+    flash(error)
+    return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('Successfully logged out')
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     init_db()
